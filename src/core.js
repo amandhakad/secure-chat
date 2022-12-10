8b2c8b29ee82960b591233a1e03427022cfb4a01
Aman Dhakad <dhakadaman6@gmail.com>
2022-12-10 21:22:23 +0530

    lobby chat working (not secure yet), to convert it to secure personal msg

--- src/core.js
+++ src/core.js
@@ -5,10 +5,11 @@ function buildLobby(lobbyKey) {
 	lpeer.on('open', function(id) {
 		console.log("lobby id", id);
 		var connArr = {};
+		var lobbyMessages = [];
 		lpeer.on('connection', function(lconn) {
 
 			lconn.on('data', function(data) {
-				console.log("Lobby received this", data);
+				// console.log("Lobby received this", data);
 
 				if(data["type"]==="new_peer") {
 					connArr[lconn.peer] = { connectionRef: lconn, updatedAt: (new Date()).getTime()};
@@ -20,7 +21,18 @@ function buildLobby(lobbyKey) {
 					// console.log("conArr update", connArr);
 				}
 
-				lconn.send({type: "PLIST", data: Object.keys(connArr)});
+				lconn.send({type: "LOBBY_DATA", data: Object.keys(connArr)});
+
+				// catch peer message and send to all peer
+				if(data["type"]==="p2lmessage") {
+					// console.log("Lobby received this", data["data"]);
+					//send to all
+					for(let con of Object.values(connArr)) {
+						console.log("trying con", con["connectionRef"]);
+						con["connectionRef"].send({ type: "l2pmessage", data: data["data"]})
+					}
+				}
+
 			});
 
 			lconn.on("close", function() {
@@ -33,31 +45,35 @@ function buildLobby(lobbyKey) {
 	});
 }
 
-function buildPeer(lobbyKey, callbackPeerInit, callbackPeerListUpdate) {
+function buildPeer(lobbyKey, resolve, callbackPeerInit, callbackPeerListUpdate, callbackMessageReceived) {
 	var peer = new Peer();
 	peer.on('open', function(id) {
 		callbackPeerInit(id);
-		connectPeerToLobby(peer, lobbyKey, callbackPeerListUpdate);
+		connectPeerToLobby(peer, lobbyKey, callbackPeerListUpdate, callbackMessageReceived, resolve);
 	});
 }
 
-function connectPeerToLobby(peer, lobbyKey, callbackPeerListUpdate) {
-	var conn = peer.connect(lobbyKey);
+function connectPeerToLobby(peer, lobbyKey, callbackPeerListUpdate, callbackMessageReceived, resolve) {
 
+	var conn = peer.connect(lobbyKey);
 	conn.on('open', function() {
 		console.log("conn", conn);
 
 		conn.on('data', function(data) {
-		 	if(data["type"]==="PLIST") {
+		 	if(data["type"]==="LOBBY_DATA") {
 		 		callbackPeerListUpdate(data["data"]);
+		 	}
+		 	if(data["type"]==="l2pmessage") {
+		 		callbackMessageReceived(data["data"]);
 		 	}
 		});
 
 		conn.send({type: "new_peer"});
-
 		remindAlive(conn);
 
+		resolve(conn);
 	});
+
 }
 
 function remindAlive(conn) {
@@ -68,7 +84,7 @@ function remindAlive(conn) {
 function expire(connArr) {
 	for(let i in connArr) {
 		if((new Date()).getTime() - connArr[i]["updatedAt"] > 5000) {
-			connArr[i]["connectionRef"].send("must close");
+			// connArr[i]["connectionRef"].send("must close");
 			connArr[i]["connectionRef"].close();
 			delete connArr[i];
 		}
